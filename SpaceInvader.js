1521888ea18d2a8c56a6ad49cf3eca3c105607a8
Asaf Benjaminov <learntpg@gmail.com>
2018-08-30 14:39:59 +0300

    Setup the main game class with a draw and update method

--- SpaceInvader.js
+++ SpaceInvader.js
@@ -1,2 +1,17 @@
 var canvas = document.getElementById("gameCanvas");
 var ctx = canvas.getContext("2d");
+
+function Draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function Update(gameTime) {
+    
+}
+
+function Play() {
+    Update();
+    Draw();
+}
+
+setInterval(Play, 10);
