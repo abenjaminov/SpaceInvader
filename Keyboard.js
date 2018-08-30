98e860c3d33a44fdd96e9fa253f25250d7278188
Asaf Benjaminov <learntpg@gmail.com>
2018-08-30 14:51:48 +0300

    Add a keyboard object

---
+++ Keyboard.js
@@ -0,0 +1,24 @@
+var Keyboard = {
+    keyCodes : {
+        Space : 32,
+        LeftArrow : 37,
+        UpArrow : 38,
+        RightArrow : 39,
+        DownArrow : 40
+    },
+    keysDown : [],
+    isKeyDown : function(keyCode) {
+        return this.keysDown[keyCode];
+    }
+}
+
+function keyDownHandler(e) {
+    Keyboard.keysDown[e.keyCode] = true;
+}
+
+function keyUpHandler(e) {
+    Keyboard.keysDown[e.keyCode] = false;
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
